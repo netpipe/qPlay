3dfc7dd106dbd7ef0a85c3ca0a5cdea0d31f8bcc
netpipe <netpipe@localhost.localdomain>
2021-12-04 17:34:49 -0700

    mp2

---
+++ extra/Qt-Media-Player2/AudioPlayer/moc_audioplayer.cpp
@@ -0,0 +1,137 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'audioplayer.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.7)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "audioplayer.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'audioplayer.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.12.7. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_AudioPlayer_t {
+    QByteArrayData data[8];
+    char stringdata0[118];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_AudioPlayer_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_AudioPlayer_t qt_meta_stringdata_AudioPlayer = {
+    {
+QT_MOC_LITERAL(0, 0, 11), // "AudioPlayer"
+QT_MOC_LITERAL(1, 12, 18), // "on_bt_stop_clicked"
+QT_MOC_LITERAL(2, 31, 0), // ""
+QT_MOC_LITERAL(3, 32, 18), // "on_bt_play_clicked"
+QT_MOC_LITERAL(4, 51, 18), // "on_bt_load_clicked"
+QT_MOC_LITERAL(5, 70, 15), // "positionChanged"
+QT_MOC_LITERAL(6, 86, 25), // "on_sl_volume_valueChanged"
+QT_MOC_LITERAL(7, 112, 5) // "value"
+
+    },
+    "AudioPlayer\0on_bt_stop_clicked\0\0"
+    "on_bt_play_clicked\0on_bt_load_clicked\0"
+    "positionChanged\0on_sl_volume_valueChanged\0"
+    "value"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_AudioPlayer[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       5,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   39,    2, 0x08 /* Private */,
+       3,    0,   40,    2, 0x08 /* Private */,
+       4,    0,   41,    2, 0x08 /* Private */,
+       5,    1,   42,    2, 0x08 /* Private */,
+       6,    1,   45,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::LongLong,    2,
+    QMetaType::Void, QMetaType::Int,    7,
+
+       0        // eod
+};
+
+void AudioPlayer::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<AudioPlayer *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_bt_stop_clicked(); break;
+        case 1: _t->on_bt_play_clicked(); break;
+        case 2: _t->on_bt_load_clicked(); break;
+        case 3: _t->positionChanged((*reinterpret_cast< qint64(*)>(_a[1]))); break;
+        case 4: _t->on_sl_volume_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject AudioPlayer::staticMetaObject = { {
+    &QMainWindow::staticMetaObject,
+    qt_meta_stringdata_AudioPlayer.data,
+    qt_meta_data_AudioPlayer,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *AudioPlayer::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *AudioPlayer::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_AudioPlayer.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int AudioPlayer::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 5)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 5;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 5)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 5;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
---
+++ extra/Qt-Media-Player2/AudioPlayer/ui_audioplayer.h
@@ -0,0 +1,175 @@
+/********************************************************************************
+** Form generated from reading UI file 'audioplayer.ui'
+**
+** Created by: Qt User Interface Compiler version 5.12.7
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_AUDIOPLAYER_H
+#define UI_AUDIOPLAYER_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QIcon>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QFormLayout>
+#include <QtWidgets/QLCDNumber>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSlider>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_AudioPlayer
+{
+public:
+    QAction *actionLoad_single_song;
+    QAction *actionLoad_Playlist;
+    QWidget *centralWidget;
+    QWidget *layoutWidget;
+    QFormLayout *formLayout;
+    QPushButton *bt_stop;
+    QPushButton *bt_play;
+    QLCDNumber *lcd;
+    QSlider *sl_tracking;
+    QLabel *lb_screen;
+    QSlider *sl_volume;
+    QLabel *label;
+    QPushButton *bt_load;
+
+    void setupUi(QMainWindow *AudioPlayer)
+    {
+        if (AudioPlayer->objectName().isEmpty())
+            AudioPlayer->setObjectName(QString::fromUtf8("AudioPlayer"));
+        AudioPlayer->resize(900, 560);
+        AudioPlayer->setMinimumSize(QSize(900, 560));
+        AudioPlayer->setMaximumSize(QSize(900, 560));
+        QIcon icon;
+        icon.addFile(QString::fromUtf8(":/imgs/icon.png"), QSize(), QIcon::Normal, QIcon::Off);
+        AudioPlayer->setWindowIcon(icon);
+        AudioPlayer->setStyleSheet(QString::fromUtf8("QMainWindow{\n"
+"background-color: rgb(0, 0, 0);\n"
+"}"));
+        actionLoad_single_song = new QAction(AudioPlayer);
+        actionLoad_single_song->setObjectName(QString::fromUtf8("actionLoad_single_song"));
+        actionLoad_Playlist = new QAction(AudioPlayer);
+        actionLoad_Playlist->setObjectName(QString::fromUtf8("actionLoad_Playlist"));
+        centralWidget = new QWidget(AudioPlayer);
+        centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
+        centralWidget->setStyleSheet(QString::fromUtf8("QWidget{\n"
+"background-color: rgb(245, 121, 0);\n"
+"}"));
+        layoutWidget = new QWidget(centralWidget);
+        layoutWidget->setObjectName(QString::fromUtf8("layoutWidget"));
+        layoutWidget->setGeometry(QRect(20, 10, 861, 563));
+        formLayout = new QFormLayout(layoutWidget);
+        formLayout->setSpacing(6);
+        formLayout->setContentsMargins(11, 11, 11, 11);
+        formLayout->setObjectName(QString::fromUtf8("formLayout"));
+        formLayout->setContentsMargins(0, 0, 0, 0);
+        bt_stop = new QPushButton(layoutWidget);
+        bt_stop->setObjectName(QString::fromUtf8("bt_stop"));
+        bt_stop->setStyleSheet(QString::fromUtf8("QPushButton{\n"
+"background-color: rgb(85, 87, 83);\n"
+"color: rgb(255, 255, 255);\n"
+"}"));
+        QIcon icon1;
+        icon1.addFile(QString::fromUtf8(":/imgs/stop.png"), QSize(), QIcon::Normal, QIcon::Off);
+        bt_stop->setIcon(icon1);
+
+        formLayout->setWidget(4, QFormLayout::LabelRole, bt_stop);
+
+        bt_play = new QPushButton(layoutWidget);
+        bt_play->setObjectName(QString::fromUtf8("bt_play"));
+        bt_play->setStyleSheet(QString::fromUtf8("QPushButton{\n"
+"background-color: rgb(85, 87, 83);\n"
+"color: rgb(255, 255, 255);\n"
+"}"));
+        QIcon icon2;
+        icon2.addFile(QString::fromUtf8(":/imgs/play.png"), QSize(), QIcon::Normal, QIcon::Off);
+        bt_play->setIcon(icon2);
+
+        formLayout->setWidget(5, QFormLayout::LabelRole, bt_play);
+
+        lcd = new QLCDNumber(layoutWidget);
+        lcd->setObjectName(QString::fromUtf8("lcd"));
+        lcd->setStyleSheet(QString::fromUtf8("QLCDNumber{\n"
+"background-color: rgb(0, 0, 0);\n"
+"color: rgb(255, 255, 255);\n"
+"}"));
+        lcd->setDigitCount(8);
+
+        formLayout->setWidget(3, QFormLayout::LabelRole, lcd);
+
+        sl_tracking = new QSlider(layoutWidget);
+        sl_tracking->setObjectName(QString::fromUtf8("sl_tracking"));
+        sl_tracking->setOrientation(Qt::Horizontal);
+
+        formLayout->setWidget(3, QFormLayout::FieldRole, sl_tracking);
+
+        lb_screen = new QLabel(layoutWidget);
+        lb_screen->setObjectName(QString::fromUtf8("lb_screen"));
+        lb_screen->setMinimumSize(QSize(760, 450));
+        lb_screen->setMaximumSize(QSize(760, 450));
+        lb_screen->setStyleSheet(QString::fromUtf8("QLabel{\n"
+"	border-color: rgb(255, 255, 255);\n"
+"	background-color: rgb(0, 0, 0);\n"
+"}"));
+        lb_screen->setPixmap(QPixmap(QString::fromUtf8(":/imgs/icon.png")));
+        lb_screen->setScaledContents(false);
+        lb_screen->setAlignment(Qt::AlignCenter);
+
+        formLayout->setWidget(5, QFormLayout::FieldRole, lb_screen);
+
+        sl_volume = new QSlider(centralWidget);
+        sl_volume->setObjectName(QString::fromUtf8("sl_volume"));
+        sl_volume->setGeometry(QRect(60, 130, 20, 311));
+        sl_volume->setValue(99);
+        sl_volume->setOrientation(Qt::Vertical);
+        label = new QLabel(centralWidget);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(40, 450, 51, 20));
+        bt_load = new QPushButton(centralWidget);
+        bt_load->setObjectName(QString::fromUtf8("bt_load"));
+        bt_load->setGeometry(QRect(40, 494, 61, 31));
+        bt_load->setStyleSheet(QString::fromUtf8("QPushButton{\n"
+"background-color: rgb(85, 87, 83);\n"
+"color: rgb(255, 255, 255);\n"
+"}"));
+        AudioPlayer->setCentralWidget(centralWidget);
+
+        retranslateUi(AudioPlayer);
+
+        QMetaObject::connectSlotsByName(AudioPlayer);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *AudioPlayer)
+    {
+        AudioPlayer->setWindowTitle(QApplication::translate("AudioPlayer", "Qt Media Player", nullptr));
+        actionLoad_single_song->setText(QApplication::translate("AudioPlayer", "Load single song", nullptr));
+#ifndef QT_NO_SHORTCUT
+        actionLoad_single_song->setShortcut(QApplication::translate("AudioPlayer", "Ctrl+O", nullptr));
+#endif // QT_NO_SHORTCUT
+        actionLoad_Playlist->setText(QApplication::translate("AudioPlayer", "Load Playlist", nullptr));
+#ifndef QT_NO_SHORTCUT
+        actionLoad_Playlist->setShortcut(QApplication::translate("AudioPlayer", "Ctrl+Shift+O", nullptr));
+#endif // QT_NO_SHORTCUT
+        bt_stop->setText(QApplication::translate("AudioPlayer", "Stop", nullptr));
+        bt_play->setText(QApplication::translate("AudioPlayer", "Play", nullptr));
+        lb_screen->setText(QString());
+        label->setText(QApplication::translate("AudioPlayer", "Volume", nullptr));
+        bt_load->setText(QApplication::translate("AudioPlayer", "Load", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class AudioPlayer: public Ui_AudioPlayer {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_AUDIOPLAYER_H
